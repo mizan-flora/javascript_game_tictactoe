4d8e4cb5ef51bf3fd7fabfaf626405835c6fcef5
mizan-flora <mizan.flora713@gmail.com>
2025-11-23 08:48:19 +0600

    updated

---
+++ tictactoe.js
@@ -0,0 +1,11 @@
+let playerO = "O";
+let playerX = "X";
+let curPlayer = playerO;
+
+let gameBoard = ["","","","","","","","",""];
+let gameCells;
+
+window.onload = function(){
+    gameCells = document.getElementsByClassName("game-cell");
+    for(let cell of )
+}
