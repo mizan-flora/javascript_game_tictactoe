c1d7de8b0f7471c34f6ad58826e24843b70590ff
mizan-flora <mizan.flora713@gmail.com>
2025-11-25 00:57:12 +0600

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,24 @@
 let playerO = "O";
 let playerX = "X";
-let curPlayer = playerO;
+let currPlayer = playerO;
 
 let gameBoard = ["","","","","","","","",""];
 let gameCells;
 
 window.onload = function(){
     gameCells = document.getElementsByClassName("game-cell");
-    for(let cell of )
+    for(let cell of gameCells){
+        cell.addEventListener("click", placeCell);
+    }
+}
+
+function placeCell(){
+
+    const index = parseInt(this.getAttribute('data-cell-index'));
+    if(gameBoard[index] != ""){
+        return;
+    }
+    gameBoard[index] = currPlayer;
+    this.innerText = currPlayer;
+    currPlayer = (currPlayer == playerO)? playerX : playerO;
 }
